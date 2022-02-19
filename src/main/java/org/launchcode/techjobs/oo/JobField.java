6f520188011e7e8fa84bfebd52c0cf98e0e22d6f
SpaceJamTim <timothyalexander95@gmail.com>
2022-02-19 01:55:00 -0600

    Working towards being DRY again...

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public abstract class JobField {
 
     public int id;
@@ -27,6 +29,20 @@ public abstract class JobField {
             return value;
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id && Objects.equals(value, jobField.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
     public String getValue() {
         return value;
     }
